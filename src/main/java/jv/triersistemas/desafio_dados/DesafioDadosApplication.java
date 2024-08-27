67eae80bd80a70e7caaed3594067b02ebee14ccf
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-08-27 15:08:25 -0300

    first commit

---
+++ src/main/java/jv/triersistemas/desafio_dados/DesafioDadosApplication.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.desafio_dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jv/triersistemas/desafio_dados/DesafioDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.desafio_dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
